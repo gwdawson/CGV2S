e900531023fc7eb66d483fbcb1cd3e99e181597f
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-09 20:47:38 +0100

    chore: add default user to sql create

--- database/create.sql
+++ database/create.sql
@@ -31,3 +31,9 @@ CREATE TABLE usercards (
   usercards_rank TEXT,
   usercards_xp INTEGER
 );
+
+INSERT INTO users (user_id, user_name, user_password, user_avatar_url)
+VALUES (1111111111111111, 'gareth', '123456', 'https://64.media.tumblr.com/24f93e6373bd3d6a27ec2e096658b865/tumblr_nwxt5iu24I1ujp48jo1_400.png');
+
+INSERT INTO cards (card_name, card_group, card_issue, card_image_url)
+VALUES ('CHAEYOUNG', 'TWICE', 0, 'https://media.discordapp.net/attachments/983643860406272060/984520837237473361/Untitled-1.png');
