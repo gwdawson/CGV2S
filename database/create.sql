bc6c301c362f4a2172360389315a1fc8a4a13147
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-10 11:39:33 +0100

    refactor: update table column names

--- database/create.sql
+++ database/create.sql
@@ -4,36 +4,36 @@ CREATE DATABASE card_game_application;
 \c card_game_application
 
 CREATE TABLE users (
-  user_id BIGINT PRIMARY KEY,
-  user_name VARCHAR(14),
-  user_password VARCHAR(64), 
-  user_avatar_url VARCHAR(255)
+  "user_id" BIGINT PRIMARY KEY,
+  "username" VARCHAR(14),
+  "password" VARCHAR(64), 
+  "avatar_url" VARCHAR(255),
+  "join_date" BIGINT,
+  "currency" INTEGER
 );
 
 CREATE TABLE cards (
-  card_id SERIAL PRIMARY KEY,
-  card_name VARCHAR(64),
-  card_group VARCHAR(64),
-  card_issue INTEGER,
-  card_image_url VARCHAR(255)
-);
-
-CREATE TABLE userdata (
-  user_id BIGINT,
-  userdata_currency INTEGER
+  "card_id" SERIAL PRIMARY KEY,
+  "name" VARCHAR(64),
+  "group" VARCHAR(64),
+  "issue_count" INTEGER,
+  "image_url" VARCHAR(255)
 );
 
 CREATE TABLE usercards (
-  user_id BIGINT,
-  card_id INTEGER,
-  usercards_id SERIAL PRIMARY KEY,
-  usercards_issue INTEGER,
-  usercards_rank TEXT,
-  usercards_xp INTEGER
+  "usercard_id" SERIAL PRIMARY KEY,
+  "user_id" BIGINT,
+  "card_id" INTEGER,
+  "issue" INTEGER,
+  "rank" TEXT,
+  "xp" INTEGER
 );
 
-INSERT INTO users (user_id, user_name, user_password, user_avatar_url)
-VALUES (1111111111111111, 'gareth', '123456', 'https://64.media.tumblr.com/24f93e6373bd3d6a27ec2e096658b865/tumblr_nwxt5iu24I1ujp48jo1_400.png');
+INSERT INTO users ("user_id", "username", "password", "avatar_url", "join_date", "currency")
+VALUES (1234567812345678, 'gareth', '123456', 'https://64.media.tumblr.com/24f93e6373bd3d6a27ec2e096658b865/tumblr_nwxt5iu24I1ujp48jo1_400.png', 0, 1000);
 
-INSERT INTO cards (card_name, card_group, card_issue, card_image_url)
-VALUES ('CHAEYOUNG', 'TWICE', 0, 'https://media.discordapp.net/attachments/983643860406272060/984520837237473361/Untitled-1.png');
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('CHAEYOUNG', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/984520837237473361/Untitled-1.png');
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (0000000000000001, 1, 1, 'H', 0);
--- database/select.sql
+++ database/select.sql
@@ -2,5 +2,4 @@
 
 SELECT * FROM users;
 SELECT * FROM cards;
-SELECT * FROM userdata;
 SELECT * FROM usercards;
