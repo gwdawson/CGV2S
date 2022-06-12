1c1ddc291bc43731f89c498bae1a09114d6d6260
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-12 16:05:43 +0100

    chore: add more cards to test account

--- database/create.sql
+++ database/create.sql
@@ -6,7 +6,7 @@ CREATE DATABASE card_game_application;
 CREATE TABLE users (
   "user_id" BIGINT PRIMARY KEY,
   "username" VARCHAR(14),
-  "password" VARCHAR(64), 
+  "password" VARCHAR(255), 
   "avatar_url" VARCHAR(255),
   "join_date" BIGINT,
   "currency" INTEGER
@@ -30,10 +30,55 @@ CREATE TABLE usercards (
 );
 
 INSERT INTO users ("user_id", "username", "password", "avatar_url", "join_date", "currency")
-VALUES (1234567812345678, 'gareth', '123456', 'https://64.media.tumblr.com/24f93e6373bd3d6a27ec2e096658b865/tumblr_nwxt5iu24I1ujp48jo1_400.png', 1654860449, 1000);
+VALUES (1234567812345678, 'gareth', '$2a$10$0JLmdp7BvXoNnxkqvV7Gz.xCZbE6.UqWoA3TG5kJGklwf5lm/HBVu', 'https://64.media.tumblr.com/24f93e6373bd3d6a27ec2e096658b865/tumblr_nwxt5iu24I1ujp48jo1_400.png', 1654860449, 1000);
 
 INSERT INTO cards ("name", "group", "issue_count", "image_url")
-VALUES ('CHAEYOUNG', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/984520837237473361/Untitled-1.png');
+VALUES ('CHAEYOUNG', 'TWICE', 16, 'https://media.discordapp.net/attachments/983643860406272060/984520837237473361/Untitled-1.png');
 
 INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
 VALUES (1234567812345678, 1, 1, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 2, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 3, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 4, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 5, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 6, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 7, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 8, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 9, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 10, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 11, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 12, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 13, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 14, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 15, 'H', 0);
+
+INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
+VALUES (1234567812345678, 1, 16, 'H', 0);
