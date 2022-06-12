e8a09e0ac1819a9b64a9289a8f85771ecad0260b
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-12 16:55:30 +0100

    feat: add more cards to database

--- database/create.sql
+++ database/create.sql
@@ -33,52 +33,28 @@ INSERT INTO users ("user_id", "username", "password", "avatar_url", "join_date",
 VALUES (1234567812345678, 'gareth', '$2a$10$0JLmdp7BvXoNnxkqvV7Gz.xCZbE6.UqWoA3TG5kJGklwf5lm/HBVu', 'https://64.media.tumblr.com/24f93e6373bd3d6a27ec2e096658b865/tumblr_nwxt5iu24I1ujp48jo1_400.png', 1654860449, 1000);
 
 INSERT INTO cards ("name", "group", "issue_count", "image_url")
-VALUES ('CHAEYOUNG', 'TWICE', 16, 'https://media.discordapp.net/attachments/983643860406272060/984520837237473361/Untitled-1.png');
+VALUES ('CHAEYOUNG', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566772142952468/chae.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 1, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('DAHYUN', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566772461723728/dahyun.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 2, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('JEONGYEON', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566772897935360/jeongyeon.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 3, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('JIHYO', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566773422227506/jihyo.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 4, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('MINA', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566773929726053/mina.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 5, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('MOMO', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566774244302888/momo.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 6, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('NAYEON', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566774533713960/nayeon.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 7, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('SANA', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566774802120785/sana.png');
 
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 8, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 9, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 10, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 11, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 12, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 13, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 14, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 15, 'H', 0);
-
-INSERT INTO usercards ("user_id", "card_id", "issue", "rank", "xp")
-VALUES (1234567812345678, 1, 16, 'H', 0);
+INSERT INTO cards ("name", "group", "issue_count", "image_url")
+VALUES ('TZUYU', 'TWICE', 1, 'https://media.discordapp.net/attachments/983643860406272060/985566775091531777/tzuyu.png');
