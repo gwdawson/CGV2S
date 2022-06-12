a02ad67db8afcd8c70d3945b7dee6f4de0121948
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-12 17:43:21 +0100

    fix: added order by for select all cards

--- models/usercards.m.js
+++ models/usercards.m.js
@@ -2,7 +2,7 @@ const database = require('../database');
 
 exports.selectUsercardsById = async (user_id) => {
   return await database.query(
-    `SELECT * FROM usercards JOIN cards ON usercards.card_id = cards.card_id WHERE user_id = $1;`,
+    `SELECT * FROM usercards JOIN cards ON usercards.card_id = cards.card_id WHERE user_id = $1 ORDER BY cards.name ASC;`,
     [user_id]
   );
 };
