3fc04d4e56efdf8a057d1bd61e40992b9909167d
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-30 17:24:29 +0100

    fix: store username as lowercase

--- models/userdata.js
+++ models/userdata.js
@@ -3,7 +3,7 @@ const database = require('../database');
 exports.insertUserdata = (userid, username, password, avatar, joindate, cards, trades, currency, packs) => {
   return database.query(
     `INSERT INTO userdata (userid, username, password, avatar, joindate, cards, trades, currency, packs) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-    [userid, username, password, avatar, joindate, cards, trades, currency, packs]
+    [userid, username.toLowerCase(), password, avatar, joindate, cards, trades, currency, packs]
   );
 };
 
