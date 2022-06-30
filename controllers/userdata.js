bcb4a6e13cfad6ef294ba5041c96b7ce3d3cbb70
Gareth Dawson <gwdawson.work@gmail.com>
2022-06-30 17:11:25 +0100

    feat: add getUserdataUserid

--- controllers/userdata.js
+++ controllers/userdata.js
@@ -1,20 +1,40 @@
-const { insertUserdata, selectUserdata, selectUserdataPassword } = require('../models/userdata');
+const {
+  insertUserdata,
+  selectUserdataUsername,
+  selectUserdataUserid,
+  selectUserdataPassword,
+} = require('../models/userdata');
 
 exports.postUserdata = async (req, res) => {
   try {
     const { userid, username, password, avatar, joindate, cards, trades, currency, packs } = req.body;
     const { rows } = await insertUserdata(userid, username, password, avatar, joindate, cards, trades, currency, packs);
-    console.log(rows[0]);
+    return res.status(200).send({ status: 200, message: rows[0] });
   } catch (e) {
     console.error(e);
     return res.status(500).send({ status: 500, message: 'Internal server error' });
   }
 };
 
-exports.getUserdata = async (req, res) => {
+exports.getUserdataUsername = async (req, res) => {
   try {
     const { username } = req.query;
-    const { rows } = await selectUserdata(username);
+    const { rows } = await selectUserdataUsername(username);
+    if (rows.length > 0) {
+      return res.status(200).send({ status: 200, message: rows[0] });
+    } else {
+      return res.status(404).send({ status: 404, message: 'User not found' });
+    }
+  } catch (e) {
+    console.error(e);
+    return res.status(500).send({ status: 500, message: 'Internal server error' });
+  }
+};
+
+exports.getUserdataUserid = async (req, res) => {
+  try {
+    const { userid } = req.query;
+    const { rows } = await selectUserdataUserid(userid);
     if (rows.length > 0) {
       return res.status(200).send({ status: 200, message: rows[0] });
     } else {
