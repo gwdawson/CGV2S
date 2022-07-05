cd088469099205fa47716597393efd6bc8b0d30d
Gareth Dawson <gwdawson.work@gmail.com>
2022-07-05 18:08:41 +0100

    fix: keep original username capitalization

--- models/userdata.js
+++ models/userdata.js
@@ -3,14 +3,14 @@ const database = require('../database');
 exports.insertUserdata = (userid, username, password, avatar, joindate, cards, trades, currency, packs) => {
   return database.query(
     `INSERT INTO userdata (userid, username, password, avatar, joindate, cards, trades, currency, packs) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9) RETURNING *`,
-    [userid, username.toLowerCase(), password, avatar, joindate, cards, trades, currency, packs]
+    [userid, username, password, avatar, joindate, cards, trades, currency, packs]
   );
 };
 
 exports.selectUserdataUsername = (username) => {
   return database.query(
-    `SELECT userid, username, avatar, joindate, cards, trades, currency, packs FROM userdata WHERE username = $1`,
-    [username]
+    `SELECT userid, username, avatar, joindate, cards, trades, currency, packs FROM userdata WHERE UPPER(username) = $1`,
+    [username.toUpperCase()]
   );
 };
 
@@ -22,5 +22,5 @@ exports.selectUserdataUserid = (userid) => {
 };
 
 exports.selectUserdataPassword = (username) => {
-  return database.query(`SELECT password FROM userdata WHERE username = $1`, [username]);
+  return database.query(`SELECT password FROM userdata WHERE UPPER(username) = $1`, [username.toUpperCase()]);
 };
